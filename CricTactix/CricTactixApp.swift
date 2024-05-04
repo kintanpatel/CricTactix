cb587d8af6007b2635f57fa100bd3a7fbcc3b216
Kintan Patel <kintanpatel4393@gmail.com>
2024-05-04 10:45:23 +0530

    Initial Commit

---
+++ CricTactix/CricTactixApp.swift
@@ -0,0 +1,17 @@
+//
+//  CricTactixApp.swift
+//  CricTactix
+//
+//  Created by kintan on 04/05/24.
+//
+
+import SwiftUI
+
+@main
+struct CricTactixApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
